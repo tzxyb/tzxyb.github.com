dedb5501a8849ff51c5d5ee02f4142bc76218635
quan88 <boguer2007@163.com>
2016-10-31 13:02:15 +0800

    df

---
+++ halloween6/js/index.js
@@ -0,0 +1,181 @@
+﻿$bili=640/1029;
+$height = $(window).height();
+$width = $height*$bili;
+var $h = $height +"px";
+$(".page").css({"width":$width,"height":$h});
+$("body, .slide3").css({"height":$h});
+var audioTap = document.getElementById('audioTap');	
+var LoadArr = [];
+LoadArr.push("images/bg.jpg","images/bg1.jpg","images/bianFu1.png","images/bianFu2.png","images/bird.png","images/bomPic.png","images/btn_again.png","images/btn_close.png","images/btn_find.png","images/btn_share.png","images/Car.png","images/eyeBg.png","images/fire.png","images/kmh.png","images/logo.png","images/menu.png","images/pkDown.png","images/pkNG.png","images/pkSZ.png","images/pkUp.png","images/quanbg.png","images/rulepic.png","images/shareText.png","images/Story1.png","images/succes.png","images/sun.png","images/text1.png","images/text2.png","images/timeLine.png","images/topPic.png","images/tree.png");
+
+/*设备的翻转状态*/
+function orientationchange() {
+    if (window.orientation == 0 || window.orientation == 180) {
+        document.getElementById("horizontal").style.display = 'none';
+        window.OBJ360 && window.OBJ360.play()
+
+    } else {
+        document.getElementById("horizontal").style.display = 'block';
+        window.OBJ360 && window.OBJ360.stop()
+    }
+};
+
+$(function(){
+
+	var FristImg = ["images/kmh.png","images/loadWitch.png","images/loadCar.png","images/wheel.png","images/phone.png"];
+		
+	var loader = new PxLoader();
+	for(var i=0; i <FristImg.length;i++ ) {
+		loader.addImage(FristImg[i]);
+	}
+	loader.start();
+	loader.addCompletionListener(function() {
+		loader = null;
+		loader = new PxLoader();
+		for(var i=0; i <LoadArr.length;i++ ) {
+		loader.addImage(LoadArr[i]);
+		}
+		loader.start();
+		loader.addProgressListener(function(e) {
+
+			var NUM = Math.ceil(e.completedCount/e.totalCount*100);
+			//alert(NUM);
+			//$(".percentN").html(NUM+"%");
+			$(".percentN").html(NUM);
+		});
+		loader.addCompletionListener(function() {
+			bipinit();
+		});
+	});
+	
+	  setTimeout(scrollTo,0,0,0);  //去除iOS和Android中的输入URL的控件条,必须放在window.onload里才能够正常的工作
+		window.onorientationchange = function() {
+		orientationchange();
+	  };
+	  
+	  document.body.addEventListener('touchmove', function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+	  }); 
+		
+	function clickTap(){audioTap.play();}
+	function bipinit(){
+	  $("#newloading").css({"display":"none"});
+	  $(".slide0").addClass("active").fadeIn();
+	 
+	 $(".btn_rule").click(function(){
+		  clickTap();
+		  $(".rule").fadeIn();
+	  });
+	  
+	  $(".btn_close").click(function(){
+		  clickTap();
+		  $(".rule").fadeOut();
+	  });
+	  
+	  $(".btn_start").click(function(){
+		  clickTap();
+		  $(".slide1").addClass("active").fadeIn();
+		  $(".slide0").removeClass("active").fadeOut();
+	  });
+	  
+	  $(".btn_share").click(function(){
+		  clickTap();
+		  $(".share").addClass("active1").fadeIn();
+	  });
+	  
+	  $(".share").click(function(){
+		  clickTap();
+		  $(this).removeClass("active1").fadeOut();
+	  });
+	  
+	  $(".btn_sub").on("click", function(){   /*提交*/
+	        clickTap();
+			var $tel=$(".telinput").val();
+			var mobile = /^1[3|4|5|7|8][0-9]\d{8}$/;
+			if(!$tel){
+				alert("请填写您的电话");
+				$(".telinput").focus();
+			}else if(!mobile.test($tel)){
+				alert("请填写完整的11位合法手机号");
+				$(".telinput").focus();
+			}else{
+				//srollTop();
+				$(".btn_sub, .telinput, .succes, .sucText").fadeOut();
+				$(".slide3 .pkUp").animate({"top":"25.5%"},1000);
+				$(".slide3 .pkDown").animate({"top":"53%"},1000);
+				setTimeout(function(){
+				   $(".quanbg").fadeIn();	
+				},1000);
+			}
+		});
+
+	}
+	
+	/*音乐控制*/
+       /*var browser = {
+			versions: function() {
+				var u = navigator.userAgent,
+					app = navigator.appVersion;
+				return {
+					webKit: u.indexOf('AppleWebKit') > -1,
+					ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+					android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1,
+					weixin: u.indexOf('MicroMessenger') > -1,
+				};
+			}(),
+			language: (navigator.browserLanguage || navigator.language).toLowerCase()
+		};*/
+		var audioAuto = document.getElementById('audio');
+		//audioAuto.play();
+		
+		var flag = 0;
+		
+		$(".music_btn").css({
+				"animation": "music_rotate 5s linear 0s infinite forwards",
+				"-webkit-animation": "music_rotate 5s linear 0s infinite forwards"
+			});
+		/*if(browser.versions.android) {
+			$(".music_btn").css({
+				"animation": "none",
+				"-webkit-animation": "none"
+			});
+			var slide0 = document.getElementById('slide0');
+			slide0.addEventListener('touchstart', function(event) {
+				audioAuto.play();
+				$(".music_btn").css({
+					"animation": "music_rotate 5s linear 0s infinite forwards",
+					"-webkit-animation": "music_rotate 5s linear 0s infinite forwards"
+				});
+				flag = 0;
+			}, false);
+		} else {
+			audioAuto.play();
+			$(".music_btn").css({
+				"animation": "music_rotate 5s linear 0s infinite forwards",
+				"-webkit-animation": "music_rotate 5s linear 0s infinite forwards"
+			});
+			flag = 0;
+		}*/
+	
+		$(".music_btn").click(function() {
+			if(flag) {
+				$(this).css({
+					"animation": "music_rotate 5s linear 0s infinite forwards",
+					"-webkit-animation": "music_rotate 5s linear 0s infinite forwards"
+				});
+				flag = 0;
+				audioAuto.play();
+			} else {
+				flag = 1;
+				audioAuto.pause();
+				$(this).css({
+					"animation": "none",
+					"-webkit-animation": "none"
+				});
+			}
+		});
+			
+			/*音乐结束*/
+		
+});
---
+++ halloween6/js/main.min3.js
@@ -0,0 +1,217 @@
+﻿var audioTap = document.getElementById('audioTap');
+var _lang = {
+	zh: {
+		title: "抓住那只南瓜",
+		score: "lv.",
+		lv_txt: ["脑残粉", "忠实粉", "路人粉", "慧眼识象牙", "火眼金睛", "洞察一切", "两眼冒光", "24k氪金眼", "已被亮瞎！"],
+	}
+},
+_config = {
+	lang: "zh",
+	color: {
+		allTime: 30,   //时间30秒
+		addTime: 0,
+		lvMap: [2, 3, 4, 5, 5, 6, 6, 7, 7, 7, 8, 8, 8, 8, 8, 8, 9,10, 11, 12]
+	}
+},
+shareData1 = {
+}; !
+function() {
+	var a = _lang[_config.lang],
+	b = $("#tpl").html(),
+	c = _.template(b, a);
+	$("#container").html(c);
+} (),
+function() {
+	var a = $("#box"),
+	b = {
+		//lv: $("#room .lv span"),
+		time: $("#room .time"),
+		start: $(".btn_again"),
+		back: $("#dialog .btn-back")
+		},
+	c = {
+		init: function(a, b, c) {
+			this.type = a,
+			this.api = API[a],
+			this.config = _config[a],
+			this.reset(),
+			this.parent = c,
+			this.el = b,
+			this.renderUI(),
+			this.inited || this.initEvent(),
+			this.inited = !0,
+			this.start()
+		},
+		renderUI: function() {
+			this.el.show()
+		},
+		initEvent: function() {
+			var d = "ontouchstart" in document.documentElement ? "touchend": "click",
+			e = this;
+			$(window).resize(function() {
+				c.renderUI()
+			}),
+			a.on(d, "img",
+			function() {
+				var a = $(this).data("type");
+				"a" == a && e.nextLv.call(e);
+			}),
+			b.start.on(d, _.bind(this.start, this)),
+			b.back.on(d, _.bind(this.back, this));
+		},
+		start: function() {
+			$(".slide2").addClass("active").fadeIn();
+		    $(".slide3").removeClass("active").fadeOut();
+			/*var  a = $(this).attr("class");
+			alert(a);
+			if(a=="btn_again"){
+				audioTap.play() ;
+			}*/
+			this._pause = !1,
+			this.lv = "undefined" != typeof this.lv ? this.lv + 1 : 0,
+			this.lvMap = this.config.lvMap[this.lv] || _.last(this.config.lvMap),
+			this.renderMap(),
+			this.renderInfo(),
+			this.timer || (this.timer = setInterval(_.bind(this.tick, this), 1e3));
+		},
+		tick: function() {
+			var timerr=(100/this.config.allTime)*(this.time-1);
+			timer2=timerr+"%";
+			return this._pause ? void 0 : (this.time--, this.time < 0 ? void this.gameOver() : void b.time.css({"background":"url(images/timeLine.png) no-repeat","background-size":""+timer2+" 100%"}), $(".timebg span").text(parseInt(this.time))/*剩余时间*/)
+		},
+		renderMap: function() {
+			if (!this._pause) {
+				var b = this.lvMap * this.lvMap,
+				c = "",
+				d = "lv" + this.lvMap;
+				_(b).times(function() {
+					c += "<img></img>"
+				}),
+				a.attr("class", d).html(c),
+				this.api.render(this.lvMap, this.lv)
+			}
+		},
+		renderInfo: function() {
+			//b.lv.text(this.lv + 1)
+		},
+		gameOver: function() {
+			var Rankstr = "";
+			var d = this.api.getGameOverText(this.lv);
+			dp_submitScore(this.lv,d.lv);
+			//this.lastLv = this.lv,
+			//this.lastGameTxt = d.txt,
+			//this.lastGamePercent = d.percent,
+			//b.d_gameover.show().find("h3").text(this.lastGameTxt),
+			
+			this._pause = !0,
+			//_hmt.push(["_trackEvent", "score", "score_" + (this.lv + 1)]),
+			this.reset()
+		},
+		reset: function() {
+			this.time = this.config.allTime,
+			this.lv = -1
+		},
+		nextLv: function() {
+			this.time += this.config.addTime,
+			//b.time.text(parseInt(this.time)),
+			this._pause || this.start()
+		},
+		back: function() {
+			this._pause = !0,
+			this.el.hide(),
+			this.parent.render()
+		}
+	};
+	window.Game = c
+} (),
+function(a) {
+	var b = {
+		index: $("#index"),
+		room: $("#room"),
+		loading: $("#loading"),
+		play: $(".btn_find")
+	},
+	c = window.navigator.userAgent.toLowerCase(),
+	d = /android/i.test(c),
+	e = /iphone|ipad|ipod/i.test(c),
+	f = {
+		init: function() {
+			this.initEvent(),
+			f.render();
+			var h = _lang[_config.lang];
+		},
+		render: function() {
+			setTimeout(function() {
+				b.loading.hide(),
+				b.index.show()
+			},
+			1e3)
+		},
+		initEvent: function() {
+			var a = "ontouchstart" in document.documentElement ? "touchstart": "click",
+			c = this;
+			b.play.on(a,
+			function() {
+				audioTap.play();
+				var a = $(this).data("type") || "color";
+				$(".slide2").addClass("active").fadeIn();
+		        $(".slide1").removeClass("active").fadeOut();
+				Game.init(a, b.room, c);
+				$(".timebg").show();
+				$(this).hide();
+			})
+		}
+	};
+	f.init(),
+	a.API = {}
+} (window),
+function() {
+	var a = $("#box"),
+	b = "img",
+	c = $("#help p"),
+	d = $("#help_color"),
+	e = {
+		lvT: _lang[_config.lang].lv_txt,
+		render: function(e, f) {
+			this.lv = f,
+			c.hide(),
+			d.show();
+			var g = _config.color.lvMap[f] || _.last(_config.color.lvMap);
+			this.d = 15 * Math.max(9 - g, 1),
+			this.d = f > 20 ? 10 : this.d,
+			this.d = f > 40 ? 8 : this.d,
+			this.d = f > 50 ? 5 : this.d;
+			var h = Math.floor(Math.random() * e * e);
+			var size = a.find(b).height() * 0.96;
+			 a.find(b).data("type", "b").attr({"src": "images/pkNG.png"}), a.find(b).eq(h).data("type", "a").attr({"src": "images/pkSZ.png"});
+		},
+		getGameOverText: function(a) {
+			var b = 20 > a ? 0 : Math.ceil((a - 20) / 10),
+			c = this.lvT[b] || _.last(this.lvT),
+			d = c + "lv" + (a + 1),
+			e = a;
+			return e = 20 > e ? 2 * a: 30 > a ? 3 * (a - 20) + 40 : 40 > a ? 1.5 * (a - 30) + 70 : 50 > a ? 1.35 * (a - 40) + 85 : 60 > a ? .05 * (a - 50) + 98.5 : 70 > a ? .02 * (a - 60) + 99 : 80 > a ? .02 * (a - 70) + 99.2 : 90 > a ? .02 * (a - 80) + 99.4 : 100 > a ? .02 * (a - 90) + 99.6 : 110 > a ? .02 * (a - 100) + 99.8 : 100,
+			e = ("" + e).length > 5 ? e.toFixed(2) : e,
+			{
+				txt: d,
+				percent: e,
+				lv:c
+			}
+		}
+	};
+	API.color = e
+} ();
+var myData = {gameid: "xyc"};
+function dp_submitScore(score) {
+                myData.score = parseInt(score);
+                  alert("我找到" + score + "个神州专车南瓜灯");
+				  //alert("游戏结束");	
+				  $(".slide3").addClass("active").fadeIn();
+		          $(".slide2").removeClass("active").fadeOut();	
+				  $(".slide3 .pkUp").css({"top":"42.5%"});
+				  $(".slide3 .pkDown").css({"top":"48%"});
+				  $(".btn_sub, .telinput, .succes, .sucText").fadeIn();
+				  $(".quanbg").fadeOut();
+				  $(".sucText").html(score);
+            }
