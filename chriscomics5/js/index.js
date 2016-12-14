254558da84db74c60f40c87aba23d1e09b781a45
quan88 <boguer2007@163.com>
2016-12-14 13:10:36 +0800

    jiaoji

--- chriscomics5/js/index.js
+++ chriscomics5/js/index.js
@@ -1,3 +1,234 @@
+var LoadArr = [];
+	LoadArr.push(
+				"images/loading_s.gif",
+				"images/loading.png",
+	            "images/sh_snow3.png",
+	            "images/sh_lu.png",
+	            "images/start_1_1.gif",
+	            "images/start_1_2.gif",
+				"images/start_1_3.gif",
+				"images/start_1_4.gif",
+				"images/start_1_5.gif",
+				"images/start_cap.gif",
+				"images/st.png",
+				"images/str.png",
+				"images/stw.png",
+				"images/strw.png",
+				"images/stb.png",
+				"images/special_1_1.gif",
+				"images/special_1_2.gif",
+				"images/special_1_3.gif",
+				"images/special_1_4.gif",
+				"images/special_1_5.gif",
+				"images/s2_sled.gif",
+				"images/s1_arm.png" ,
+				"images/bg_s1.png",
+				"images/s1w.png",
+				"images/s1r_arm.png",
+				"images/bg_s1r.png",
+				"images/s1r_car.png",
+				"images/s1rw.png",
+				"images/s2_sled.gif",
+				"images/s2_dot.png",
+				"images/s2r_arm.png",
+				"images/s3_lu_angry.gif",
+				"images/bg_s3.gif",
+				"images/s3_arm.png",
+				"images/s3w.png",
+				"images/s1r_car.png",
+				"images/s3r_ren.gif",
+				"images/s3rw.png",
+				"images/uniform_1_1.gif",
+				"images/uniform_1_2.gif",
+				"images/uniform_1_3.gif",
+				"images/uniform_1_4.gif",
+				"images/uniform_1_5.gif",
+				"images/elk_1_1.gif",
+				"images/elk_1_2.gif",
+				"images/elk_1_3.gif",
+				"images/elk_1_4.gif",
+				"images/elk_1_5.gif",
+				"images/christmas_1_1.gif",
+				"images/christmas_1_2.gif",
+				"images/christmas_1_3.gif",
+				"images/christmas_1_4.gif",
+				"images/christmas_1_5.gif",
+				"images/old_1_1.gif",
+				"images/old_1_2.gif",
+				"images/old_1_3.gif",
+				"images/old_1_4.gif",
+				"images/old_1_5.gif",
+				"images/sleep_1_1.gif",
+				"images/sleep_1_2.gif",
+				"images/sleep_1_3.gif",
+				"images/sleep_1_4.gif",
+				"images/sleep_1_5.gif",
+				"images/arrow.png",
+				"images/sh_snow3.png",
+				"images/sh_snow3.png",
+				"images/sh_lu.png",
+				"images/phone.png",
+				"images/bg_s2r.gif",
+				"images/music_on.png",
+				"images/music_off.png",
+				"images/bg_u1r.png",
+				"images/s1r_car.png",
+				"images/u1_sled.png",
+				"images/s1_arm.png",
+				"images/bg_s1.png",
+				"images/u1w.png",
+				"images/u1_sled.png",
+				"images/u2.png",
+				"images/u2_line.png",
+				"images/bg_u2r.png",
+				"images/s1r_car.png",
+				"images/u2rw.png",
+				"images/u1_sled.png",
+				"images/bg_u3.gif",
+				"images/u3_line.png",
+				"images/u3w.png",
+				"images/bg_u3r.png",
+				"images/s1r_car.png",
+				"images/u3rw.png",
+				"images/e1.gif",
+				"images/e1_lu.png",
+				"images/e1_fire.png",
+				"images/e1w.png",
+				"images/e1r.gif",
+				"images/s1r_car.png",
+				"images/e1r_line.png",
+				"images/e2.gif",
+				"images/e1_lu.png",
+				"images/e2_arm.png",
+				"images/e2r.png",
+				"images/e2r_arm1.png",
+				"images/e2r_arm2.png",
+				"images/e2r_arm3.png",
+				"images/e2r_arm4.png",
+				"images/s1r_car.png",
+				"images/e2rw.png",
+				"images/e3.gif",
+				"images/e3_lu.png",
+				"images/e3_why.png",
+				"images/e3r.png",
+				"images/s1r_car.png",
+				"images/e3rw.png",
+				"images/c1.png",
+				"images/c1_arm.png",
+				"images/c1w.png",
+				"images/s1r_car.png",
+				"images/c1r_arm.png",
+				"images/c1r.png",
+				"images/c1rw.png",
+				"images/u1_sled.png",
+				"images/c2_line1.png",
+				"images/c2_line2.png",
+				"images/c2_star1.png",
+				"images/c2_star2.png",
+				"images/c2_tuan.png",
+				"images/u1_sled.png",
+				"images/c3_lu.png",
+				"images/c3.gif",
+				"images/c3_line.png",
+				"images/c3w.png",
+				"images/s1r_car.png",
+				"images/c3r.gif",
+				"images/c3r_line.png",
+				"images/c3rw.png",
+				"images/bg.mp3",
+				"images/o1.png",
+				"images/o1_r.png",
+				"images/o1_lu.png",
+				"images/o1_music.png",
+				"images/o1r.png",
+				"images/o1rw.png",
+				"images/bg_step.png",
+				"images/o1.png",
+				"images/o2_r.png",
+				"images/o2_lu.png",
+				"images/o2_why.png",
+				"images/o2w.png",
+				"images/o2r_arm.png",
+				"images/o2r.png",
+				"images/o2rw.png",
+				"images/bg_step.png",
+				"images/o1.png",
+				"images/o3_r.png",
+				"images/o3_lu.png",
+				"images/o3w.png",
+				"images/o3r.png",
+				"images/sl1_r.png",
+				"images/sl1_arm.png",
+				"images/sl1_lu.png",
+				"images/sl1_zzz.png",
+				"images/sl1w.png",
+				"images/sl1r.png",
+				"images/sl1rw.png",
+				"images/bg_step.png",
+				"images/sl2_r.png",
+				"images/sl1_lu.png",
+				"images/sl2_sudor.png",
+				"images/sl1_zzz.png",
+				"images/sl2w.png",
+				"images/sl2r.png",
+				"images/bg_step.png",
+				"images/sl3_r.png",
+				"images/sl3_lu.png",
+				"images/sl3_star.png",
+				"images/sl3w.png",
+				"images/sl3r.png",
+				"images/sl3rw.png",
+				"images/g_peo.png",
+				"images/glw.png",
+				"images/grw.png",
+				"images/bg_tel.png",
+				"images/v_tel.png",
+				"images/btn_again.png",
+				"images/btn_share.png",
+				"images/sh_snow3.png",
+				"images/sh_snow3.png",
+				"images/sh_lu.png" ,
+				"images/g_title.png",
+				"images/g_quan.png",
+				"images/btn_dld.png",
+				"images/g_close.png"
+			);
+var _w = $(window).width();
+var _h = $(window).height();
+var bili = 640 / 1029;
+$(".slide").css({
+	"height": _h + "px"
+});
+$(".slide .wrap").css({
+	"-webkit-transform" :"scale("+_h/1029+")",
+	"transform" :"scale("+_h/1029+")",
+	"left": (_w - _h*bili)/2+"px"
+});
+
+setTimeout(scrollTo,0,0,0);  //去除iOS和Android中的输入URL的控件条,必须放在window.onload里才能够正常的工作
+  window.onorientationchange = function() {
+  orientationchange();
+};
+
+document.body.addEventListener('touchmove', function(e) {
+	  //e.stopPropagation();
+	 // e.preventDefault();
+});
+
+var loader = new PxLoader();
+for(var i=0; i <LoadArr.length;i++ ) {
+	loader.addImage(LoadArr[i]);
+}
+
+loader.start();
+loader.addCompletionListener(function() {
+	    bipinit();
+});
+function bipinit() {
+	var audioAuto = document.getElementById('audio');
+	$("#newloading").css({"display":"none"});
+	$('.slide-1').removeClass("hide").addClass('current-slide');
+};
 (function(){
 
 var now = { row:1 }, last = { row:0};
@@ -58,60 +289,6 @@ function pageMove(tw){
 	isAnimating = false;
 	
 }
-
 })();
 
 
-$('#music-btn').on('touchstart', function() {
-	if ($(this).hasClass('on')) {
-		$('audio').get(0).pause();
-		$(this).addClass('off').removeClass('on');
-	} else {
-		$('audio').get(0).play();
-		$(this).removeClass('off').addClass('on');
-	}
-});
-var parts={
-	start: {
-		'img':['1_1','1_2','1_3','1_4','1_5','cap'],
-		'animate':['fadeIn','fadeIn','fadeIn','slideLeft','slideRight','cap'],
-		'delay':['0.1','0.4','0.9','1.2','1.5']
-	},
-	startStory: {
-		
-	},
-	special: {
-	},
-	specialCar: {
-		
-	},
-	uniform:{
-	},
-	uniformChapter:{
-		
-	},
-	elk:{
-	},
-	elkArticles:{
-		
-	},
-	christmas:{
-	},
-	christmasKnot:{
-		
-	},
-	old:{
-	},
-	oldDriver:{
-		
-	},
-	sleep:{
-	},
-	sleepArticle:{
-		
-	}
-	
-};
-var chriscomics={
-		
-};
